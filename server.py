907375cd1c3e0864faf8e61886ccb7efa7139935
celestialbunny <coronateh@outlook.com>
2019-02-19 11:16:36 +0800

    set the individual class pages to display own database collection along with select options

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import peeweedbevolve
 from flask import (Flask, render_template, request, flash, redirect, url_for)
 from models import db, Store, Warehouse, Product
+from peewee import *
 
 # python -c 'import os; print(os.urandom(16))'
 # b'_5#y2L"F4Q8z\n\xec]/
@@ -36,29 +37,60 @@ def contact():
 @app.route("/store", methods=['GET', 'POST'])
 def store():
 	# form = forms.Store()
+	# store_list = Store.select()
+	store_list = Store.select(Store.name)
 	if request.method == 'POST':
 		# How to post a success message from form submission if not using flash but using bootstrap under 'store.html' line 5
 		# breakpoint()
 		new_store = Store(name=request.form['store_name'])
 		new_store.save()
 		flash("Store created", "success")
-		return redirect(url_for('store'))
+		return redirect(url_for('store'), store_list)
 	else:
-		return render_template('store.html')
+		return render_template('store.html', store_list=store_list)
+
+@app.route("/store/<int:store_number>")
+def store_info(store_number):
+	store = Store.get_by_id(store_number)
+	print(store.name)
+	warehouse_count = Warehouse.select(fn.COUNT(Warehouse.store.id == Store.id)).join(Store)
+	# Need to insert data in the Warehouse before able to proceed with the query
+	for result in warehouse_count:
+		print(result)
+	context = {'store': store, 'warehouse_count': warehouse_count}
+	return render_template('store_page.html', **context)
 
 @app.route("/warehouse", methods=['GET', 'POST'])
 def warehouse():
-	store_list = Store.select()
 	if request.method == 'POST':
-		# How to post a success message from form submission if not using flash but using bootstrap under 'store.html' line 5
-		# breakpoint()
-		store = Store.get_by_id(request.form['store_id'])
-		new_warehouse = Warehouse(location=request.form['warehouse_location'], store=store)
+		store = request.form['store_list']
+		location = request.form['warehouse_location']
+		context = {'store': store, 'location': location}
+		new_warehouse = Warehouse(location=location, store=store)
 		new_warehouse.save()
 		flash("Warehouse created", "success")
 		return redirect(url_for('warehouse'))
 	else:
-		return render_template('warehouse.html', store_list=store_list)
+		# result = Store.select(Store.name, fn.COUNT(Warehouse.store.id == Store.id)).join(Warehouse).group_by(Store.name).where(Store.id == Warehouse.store.id)
+		warehouse_list = Warehouse.select()
+		store_list = Store.select()
+		return render_template('warehouse.html', warehouse_list=warehouse_list, store_list=store_list)
+
+@app.route("/product", methods=['GET', 'POST'])
+def product():
+	if request.method == 'POST':
+		name = request.form['product_name']
+		description = request.form['product_description']
+		warehouse = request.form['warehouse_list']
+		color = request.form['product_color']
+		new_product = Product(name=name, description=description, warehouse=warehouse, color=color)
+		new_product.save()
+		flash("Product created", "success")
+		return redirect(url_for('product'))
+	else:
+		product_list = Product.select()
+		warehouse_list = Warehouse.select()
+		return render_template('product.html', warehouse_list=warehouse_list, product_list=product_list)
 
 if __name__ == '__main__':
 	app.run()
